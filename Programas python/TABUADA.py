ac9938b43f2d395bc25a26566957eff859150596
Iomadson-Santos <iomadsonsantos@gmail.com>
2026-01-21 16:59:00 -0300

    Add files via upload

---
+++ Programas python/TABUADA.py
@@ -0,0 +1,10 @@
+#TABUADA#
+numero = int(input("Digite o número que deseja a tabuada: "))
+
+i = 1
+
+
+while i <= 20:
+    print(f"{numero} X {i}= {numero*i}")
+    i = i + 1 #Incremento de 1 em i
+    print("Fim do programa")
